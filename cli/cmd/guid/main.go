f961c8d7f6eea95a147f214954f8791614aab58d
Steve High <steve@thentwrk.com>
2022-03-17 12:41:25 -0400

    guid

---
+++ cli/cmd/guid/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/ntwrk1/guid"
+)
+
+var (
+	prefix string
+)
+
+func main() {
+	flag.StringVar(&prefix, "prefix", "", "byte prefix")
+	flag.Parse()
+
+	if len(prefix) >= 2 {
+		guid.SetGlobalPrefixBytes(prefix[0], prefix[1])
+	}
+
+	g, _ := guid.New()
+	
+	fmt.Println(g.String())
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/google/uuid v1.1.1
+	github.com/ntwrk1/guid v0.0.0-20210203171958-d8e4c787f7b9 // indirect
 	github.com/schigh/str v0.0.0-20170715183927-361193c512d2
 	github.com/stretchr/testify v1.6.1 // indirect
 )
